5648929d8bc2a0fca4fbd8a5aacfd996cfe219fd
ccy <ccy.com>
2024-04-08 16:43:18 +0800

    achieve ThreadPool.cc

--- ThreadPool.cc
+++ ThreadPool.cc
@@ -182,5 +182,85 @@ bool ThreadPool::isInit() const{
     return is_init_;
 }
 
+Status ThreadPool::releaseSecondaryThread(int size){
+    Status status;
+    LOCK_GUARD lock(st_mutex_);
+    // 将所有已经结束的，删掉
+    for(auto iter = secondary_threads_.begin(); iter != secondary_threads_.end();){
+        !(*iter)->done_? secondary_threads_.erase(iter++) : iter++;
+    }
+    RETURN_ERROR_STATUS_BY_CONDITION((size > secondary_threads_.size()), \
+                "cannot release [" + std::to_string(size) + "] secondary thread,"    \
+                + "only [" + std::to_string(secondary_threads_.size()) + "] left.")
+
+    // 标记需要删除的信息
+    for(auto iter = secondary_threads_.begin(); iter != secondary_threads_.end() && size-- > 0;)
+    {
+        (*iter)->done_ = false;
+        iter++;
+    }
+    return status;
+}
+
+int ThreadPool::dispatch(int origIndex){
+    int realIndex = 0;
+    if(DEFAULT_TASK_STRATEGY == origIndex){
+        /**
+         * 如果是默认策略信息，在[0, default_thread_size_) 之间的，通过 thread 中queue来调度
+         * 在[default_thread_size_, max_thread_size_) 之间的，通过 pool 中的queue来调度
+         */
+        realIndex = cur_index_++;
+        if(cur_index_ >= config_.max_thread_size_ || cur_index_ < 0){
+            cur_index_ = 0;
+        }
+    }else{
+        realIndex = origIndex;
+    }
+    return realIndex;         // 交到上游去判断，走哪个线程
+}
+
+Status ThreadPool::createSecondaryThread(int size){
+    Status status;
+    int leftSize = (int)(config_.max_thread_size_- config_.default_thread_size_ - secondary_threads_.size());
+    int realSize = std::min(size, leftSize);
+
+    LOCK_GUARD lock(st_mutex_);
+    for(int i = 0; i < realSize; i++){
+        auto ptr = MAKE_UNIQUE_OBJECT(ThreadSecondary)
+        ptr->setThreadPoolInfo(&task_queue_, &priority_task_queue_, &config_);
+        status += ptr->init();
+        secondary_threads_.emplace_back(std::move(ptr));
+    }
+
+    return status;
+}
+
+void ThreadPool::monitor(){
+    while(config_.monitor_enable_){
+        while(config_.monitor_enable_ && !is_init_){
+            // 如果没有init，则一直处于空跑状态
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        
+        auto span = config_.monitor_span_;
+        while(config_.monitor_enable_ && is_init_ && span--){
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        // 若 primary线程都在执行，则表示忙碌
+        bool busy = !primary_threads_.empty() && std::all_of(primary_threads_.begin(), primary_threads_.end(),
+                                [](ThreadPrimaryPtr ptr) { return nullptr != ptr && ptr->is_running_; });
+
+        LOCK_GUARD lock(st_mutex_);
+        if(busy || !priority_task_queue_.empty()){
+            createSecondaryThread(1);
+        }
+        
+        // 判断 secondary 线程是否需要退出
+        for (auto iter = secondary_threads_.begin(); iter != secondary_threads_.end(); ) {
+            (*iter)->freeze() ? secondary_threads_.erase(iter++) : iter++;
+        }
+    }
+}
 
 }
